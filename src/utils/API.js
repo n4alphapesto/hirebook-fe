dfc796259625697f0d353d115071d64160a4de48
raj tejani <rajtejani9@gmail.com>
2021-07-20 14:53:20 +0530

    changed host to localhost

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://192.168.1.5:3000/api/",
+  baseURL: "http://localhost:3000/api/",
 });
 
 const publicRoutes = ["/auth/login", "/auth/register"];
