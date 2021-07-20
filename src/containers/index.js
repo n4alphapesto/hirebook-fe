3cfe6ea07fdb95cfb2a043d5013d89673b19963b
Raj Tejani <rajtejani9@gmail.com>
2021-07-20 18:54:49 +0530

    imported components

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,12 +1,12 @@
 import LandingPage from "./LandingPage";
+import RecruiterOnboarding from "./RecruiterOnboarding";
+import JobSeekerOnboarding from "./JobSeekerOnboarding";
 import JobseekerDashboard from "./JobseekerDashboard/JobseekerDashboard";
 import RecruiterDashboard from "./RecruiterDashboard/RecruiterDashboard";
 import RecruiterJobPostForm from "./RecruiterDashboard/RecruiterJobPostForm";
 import RecruiterProfile from "./RecruiterDashboard/RecruiterProfile";
 import RecruiterJobPostDetails from "./RecruiterDashboard/RecruiterJobPostDetails";
 import RecruiterViewJobSeekerDetails from "./RecruiterDashboard/RecruiterViewJobSeekerDetails";
-import RecruiterOnboarding from "./RecruiterOnboarding";
-import JobSeekerOnboarding from "./JobSeekerOnboarding";
 
 export {
   LandingPage,
